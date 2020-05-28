4fdffed252e350274dc2f922b9556278d420b3ea
Maruf <maruf@terra.money>
2020-05-28 09:01:49 +0600

    Updated the total account query (use exixting data)

--- src/scripts/populateDashboard.ts
+++ src/scripts/populateDashboard.ts
@@ -1,13 +1,15 @@
-import { getRepository } from 'typeorm'
-import { startOfDay } from 'date-fns'
+import { getRepository, getConnection } from 'typeorm'
+import { startOfDay, startOfToday } from 'date-fns'
 
 import { DashboardEntity, init as initORM } from 'orm'
 import {
   getAccountCountByDay,
   getBlockRewardsByDay,
   getStakingReturnByDay,
-  getTxVolumeByDay
+  getTxVolumeByDay,
+  getDateFromDateTime
 } from 'collector/dashboard'
+import { getQueryDateTime } from 'lib/time'
 import config from 'config'
 
 async function getDashboard(datetime: Date): Promise<DashboardEntity | undefined> {
@@ -17,10 +19,66 @@ async function getDashboard(datetime: Date): Promise<DashboardEntity | undefined
   })
   return dashboard
 }
+// NB: we are using the general infos total account count for populating dashboard because
+// getting the total count for everyday from account_tx table is a costly operation which leads to long time.
+
+async function getDailyTotalAccounts(): Promise<{ date: string; total_account_count: number }[]> {
+  const rawQuery = `SELECT MAX(total_account_count) AS total_account_count, DATE(datetime) as date 
+    FROM general_info WHERE datetime < '${getQueryDateTime(startOfToday())}' GROUP BY date ORDER BY date ASC`
+
+  const result: {
+    total_account_count: number
+    date: string
+  }[] = await getConnection().query(rawQuery)
+  return result
+}
+
+async function getDailyActiveAccounts(): Promise<{ date: string; active_account_count: number }[]> {
+  // EXP: we are using count (SELECT DISTINCT account FROM x) rather COUNT(DISTINCT account) because its is 10 times faster.
+  const subQuery = `SELECT DISTINCT account, DATE(timestamp) AS date FROM account_tx WHERE timestamp < '${getQueryDateTime(
+    startOfToday()
+  )}'`
+
+  const rawQuery = `SELECT COUNT(*) AS active_account_count, t.date AS date FROM (${subQuery}) AS t GROUP BY t.date ORDER BY t.date ASC`
+  const result: {
+    date: string
+    active_account_count: number
+  }[] = await getConnection().query(rawQuery)
+  return result
+}
+
+async function accountCountHistory(): Promise<{
+  [date: string]: {
+    activeAccount: number
+    totalAccount: number
+  }
+}> {
+  const totalAccount = await getDailyTotalAccounts()
+  const activeAccount = await getDailyActiveAccounts()
+
+  const totalAccountMap = totalAccount.reduce((acc, info) => {
+    const dateKey = getDateFromDateTime(new Date(info.date))
+    acc[dateKey] = info.total_account_count
+    return acc
+  }, {})
+
+  const res = activeAccount.reduce((acc, info) => {
+    const dateKey = getDateFromDateTime(new Date(info.date))
+    if (totalAccountMap[dateKey]) {
+      acc[dateKey] = {
+        activeAccount: info.active_account_count,
+        totalAccount: totalAccountMap[dateKey]
+      }
+    }
+    return acc
+  }, {})
+  return res
+}
 
 async function populateDashboard() {
   await initORM()
-  const accountGrowth = await getAccountCountByDay()
+  const accountGrowth = await accountCountHistory()
+  console.log(accountGrowth)
 
   for (const dateKey of Object.keys(accountGrowth)) {
     const date = startOfDay(dateKey)
