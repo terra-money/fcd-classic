829fd8d5f00f93f8ef2e468b2ee37db4a1008c64
Maruf <maruf@terra.money>
2020-05-29 13:03:11 +0600

    Use sql function for query

--- src/collector/block/block.ts
+++ src/collector/block/block.ts
@@ -14,7 +14,7 @@ import { setReward } from 'collector/reward'
 import { setSwap } from 'collector/swap'
 import { setNetwork } from 'collector/network'
 
-function getTxHashesFromBlock(block): string[] {
+export function getTxHashesFromBlock(block): string[] {
   const numTxs = Number(get(block, 'block.header.num_txs'))
 
   if (!numTxs || numTxs === 0) {
--- src/service/transaction/getTx.ts
+++ src/service/transaction/getTx.ts
@@ -2,10 +2,10 @@ import { TxEntity } from 'orm'
 import { getRepository } from 'typeorm'
 
 export async function getTx(txhash: string): Promise<Transaction.LcdTransaction | undefined> {
-  const tx = await getRepository(TxEntity)
+  const qb = getRepository(TxEntity)
     .createQueryBuilder()
-    .where(`hash = :hash`, { hash: txhash.toLowerCase() })
-    .orWhere(`hash = :hash`, { hash: txhash.toUpperCase() })
-    .getOne()
-  return tx && (tx[0].data as Transaction.LcdTransaction)
+    .where('hash = lower(:txhash) OR hash=upper(:txhash) ', { txhash })
+
+  const tx = await qb.getOne()
+  return tx && (tx.data as Transaction.LcdTransaction)
 }
