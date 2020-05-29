4bf70534d76085793b23e19519beb3ea2ce1a307
Paul Kim <paul@terra.money>
2020-05-29 16:17:17 +0900

    chore(service/transaction): remove space

--- src/service/transaction/getTx.ts
+++ src/service/transaction/getTx.ts
@@ -4,7 +4,7 @@ import { getRepository } from 'typeorm'
 export async function getTx(txhash: string): Promise<Transaction.LcdTransaction | undefined> {
   const qb = getRepository(TxEntity)
     .createQueryBuilder()
-    .where('hash = lower(:txhash) OR hash=upper(:txhash) ', { txhash })
+    .where('hash=lower(:txhash) OR hash=upper(:txhash) ', { txhash })
 
   const tx = await qb.getOne()
   return tx && (tx.data as Transaction.LcdTransaction)
