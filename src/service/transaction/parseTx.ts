1e473df14713bb5fb3b00397fc73faaf2d403408
Maruf <maruf@terra.money>
2020-06-10 17:54:06 +0600

    Add tax info in parsed tx list

--- src/service/transaction/parseTx.ts
+++ src/service/transaction/parseTx.ts
@@ -2,10 +2,6 @@ import * as Bluebird from 'bluebird'
 import { get, filter } from 'lodash'
 import parseMsg from './parseMsg'
 
-function addLogsToMsg(msg: Transaction.Message, logs: Transaction.Log[]): void {
-  msg['log'] = logs && logs.shift()
-}
-
 function isSuccessful(logs): { success: boolean; errorMessage?: string } {
   const failed = filter(logs, { success: false })
   return !logs || failed.length > 0
@@ -42,21 +38,24 @@ function failedRawLogToLogs(
 export default function parseMsgsInTx(account: string | undefined) {
   return async (tx): Promise<ParsedTxInfo> => {
     const msgs = get(tx, 'data.tx.value.msg') as Transaction.Message[]
-    const logs: Transaction.Log[] = tx.data.logs ? tx.data.logs : failedRawLogToLogs(get(tx, 'data.raw_log'))
+    const logs: Transaction.Log[] = tx.data.logs ? get(tx, 'data.logs') : failedRawLogToLogs(get(tx, 'data.raw_log'))
     const chainId = get(tx, 'chain_id')
     const { success, errorMessage } = isSuccessful(logs)
 
+    const tax = get(logs[0], 'log.tax')
+
     const parsedMsgs: ParsedTxMsgInfo[] = await Bluebird.map(msgs, (msg) => {
-      addLogsToMsg(msg, logs)
+      // index 0 is tax info
+      msg['log'] = logs && logs.filter((_, index) => index > 0)
       return parseMsg(msg, account, success)
     })
-
     return {
       timestamp: get(tx, 'data.timestamp'),
       txhash: get(tx, 'data.txhash'),
       msgs: parsedMsgs,
       txFee: get(tx, 'data.tx.value.fee.amount'),
       memo: get(tx, 'data.tx.value.memo'),
+      tax,
       success,
       errorMessage,
       chainId
