ed085d74153676230be115bf7230f81c1e8d7670
Maruf <maruf@terra.money>
2020-07-28 10:13:06 +0600

    Drop similar variable

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -8,4 +8,3 @@ export const MOVING_AVG_WINDOW_IN_DAYS = 7
 export const DAYS_IN_YEAR = 365
 export const CORS_REGEXP = /^https:\/\/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.){0,3}(terra\.money|terra\.dev|station\.terra-project\.now\.sh)(?::\d{4,5})?(?:\/|$)/
 export const ONE_DAY_IN_MS = 60000 * 60 * 24
-export const CORS_REGEXP = /^https:\/\/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.){0,3}(terra\.money|terra\.dev|station\.terra-project\.now\.sh)(?::\d{4,5})?(?:\/|$)/
