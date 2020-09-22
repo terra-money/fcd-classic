9525aaae60d744a23cbb11e3aaa2c1ffbcefceb1
Paul Kim <paul@terra.money>
2020-09-22 23:07:23 +0900

    Index wasm msgs to account tx (#90)
    * feat(collector): save wasm txs to AccountTxEntity
    * feat(TransactionController): add wasm to action param
    * upgrade module dependencies
    * use official apidoc-swagger

--- src/service/staking/getValidatorReturn.ts
+++ src/service/staking/getValidatorReturn.ts
@@ -7,11 +7,11 @@ SELECT operator_address,
   COUNT(*) AS data_point_count
 FROM validator_return_info
 WHERE TIMESTAMP >= DATE(NOW() - INTERVAL '30 day')
-AND operator_address = '${operatorAddress}'
+AND operator_address = $1
 AND avg_voting_power > 0
 GROUP BY operator_address;`
 
-  const validatorReturn = await getConnection().query(rawQuery)
+  const validatorReturn = await getConnection().query(rawQuery, [operatorAddress])
 
   if (validatorReturn.length < 1) {
     return {
@@ -19,6 +19,7 @@ GROUP BY operator_address;`
       isNewValidator: true
     }
   }
+
   return {
     stakingReturn: validatorReturn[0].annual_return,
     isNewValidator: validatorReturn[0].data_point_count > 15 ? false : true
