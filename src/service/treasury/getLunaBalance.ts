b906fd763130a61373328c4ead7c8eb9551ac95c
Paul Kim <paul@terra.money>
2020-12-23 00:23:26 +0900

    refactor(treasury): getLunaBalance

--- src/service/treasury/getLunaBalance.ts
+++ src/service/treasury/getLunaBalance.ts
@@ -2,7 +2,6 @@ import * as lcd from 'lib/lcd'
 import BigNumber from 'bignumber.js'
 
 async function getLunaBalance(address: string): Promise<string> {
-  let amount = new BigNumber(0)
   const [account, delegations, unbondings] = await Promise.all([
     lcd.getAccount(address),
     lcd.getDelegations(address),
@@ -10,25 +9,14 @@ async function getLunaBalance(address: string): Promise<string> {
   ])
 
   const coin = (account as LazyVestingAccount).value.coins?.find((c) => c.denom === 'uluna')
-
-  if (coin) {
-    // console.log(`bank: ${coin.amount}`)
-    amount = amount.plus(coin.amount)
-  }
-
-  delegations.forEach((d) => {
-    // console.log(`delegation: ${d.balance.amount} ${d.validator_address}`)
-    amount = amount.plus(d.balance.amount)
-  })
-
-  unbondings.forEach((u) =>
-    u.entries.forEach((e) => {
-      // console.log(`unbonding: ${e.balance}`)
-      amount = amount.plus(e.balance)
-    })
+  const bankAmount = new BigNumber(coin?.amount || 0)
+  const delegationsAmount = delegations.reduce((prev, curr) => prev.plus(curr.balance.amount), new BigNumber(0))
+  const unbodingsAmount = unbondings.reduce(
+    (prev, curr) => curr.entries.reduce((pe, ce) => pe.plus(ce.balance), prev),
+    new BigNumber(0)
   )
 
-  return amount.toString()
+  return bankAmount.plus(delegationsAmount).plus(unbodingsAmount).toString()
 }
 
 export default getLunaBalance
