b2968636a2d6d01d2d369103ebb51e44c66fe40e
Paul Kim <paul@terra.money>
2021-05-27 06:54:22 +0900

    fix(orm): wasm code relation

--- src/orm/WasmContractEntity.ts
+++ src/orm/WasmContractEntity.ts
@@ -39,9 +39,6 @@ export default class WasmContractEntity {
   @ManyToOne(() => WasmCodeEntity, (code) => code, {
     eager: true
   })
-  @JoinColumn([
-    { name: 'code_id', referencedColumnName: 'codeId' },
-    { name: 'chain_id', referencedColumnName: 'chainId' }
-  ])
+  @JoinColumn([{ name: 'code_id', referencedColumnName: 'codeId' }])
   code: WasmCodeEntity
 }
