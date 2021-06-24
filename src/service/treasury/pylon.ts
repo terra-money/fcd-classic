d1226ddce9ed69b23ab4201a0a86f92a37af664e
ByeongSu Hong <frostornge@gmail.com>
2021-06-24 15:38:25 +0900

    feat: support $MINE token  (#121)
    * feat(service/treasury): add support MINE token
    * chore(docs): prettify readme & add env var docs
    * chore(docs): fix typo

---
+++ src/service/treasury/pylon.ts
@@ -0,0 +1,40 @@
+import * as Bluebird from 'bluebird'
+import BigNumber from 'bignumber.js'
+import * as rp from 'request-promise'
+import * as lcd from 'lib/lcd'
+import config from 'config'
+import memoizeCache from 'lib/memoizeCache'
+import { getTotalSupply } from './token'
+
+interface PylonOverview {
+  tokenAddress: string
+  priceInUst: number
+  totalStaked: string
+  circulatingSupply: string
+}
+
+async function _getOverview(): Promise<PylonOverview | undefined> {
+  const res = await rp(`${config.PYLON_API_ENDPOINT}/mine/v1/overview`, {
+    method: 'GET',
+    json: true
+  })
+
+  if (!res?.data?.tokenAddress) {
+    return undefined
+  }
+  if (typeof res.data.tokenAddress !== 'string') {
+    return undefined
+  }
+
+  return {
+    tokenAddress: res.data.tokenAddress || '',
+    priceInUst: res.data.priceInUst || 0.0,
+    totalStaked: res.data.totalStaked ? res.data.totalStaked.toString() : '',
+    circulatingSupply: res.data.circulatingSupply ? res.data.circulatingSupply.toString() : ''
+  } as PylonOverview
+}
+
+export const getOverview = memoizeCache(_getOverview, {
+  promise: true,
+  maxAge: 5 * 60 * 1000 /* 5 minutes */
+})
