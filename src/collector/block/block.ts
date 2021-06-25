c2546415b661dcc0bc219eb9b16125a3ff10e4f6
Paul Kim <paul@terra.money>
2021-06-25 16:28:57 +0900

    fix(colllector/block): upsert duplicated tx

--- src/collector/block/block.ts
+++ src/collector/block/block.ts
@@ -131,6 +131,7 @@ export async function saveBlockInformation(
       const newBlockEntity = await mgr
         .getRepository(BlockEntity)
         .save(getBlockEntity(newBlockHeight, lcdBlock, newBlockReward))
+
       // get block tx hashes
       const txHashes = lcd.getTxHashesFromBlock(lcdBlock)
 
