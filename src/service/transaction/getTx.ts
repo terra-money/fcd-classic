cc322522148b33ee863da4ca04911619c6e9f20e
Paul Kim <paul@terra.money>
2021-08-24 16:08:43 +0900

    feat(bank): sequence adjustment feat(tx): mempool tx search

--- src/service/transaction/getTx.ts
+++ src/service/transaction/getTx.ts
@@ -1,7 +1,31 @@
+import Mempool from 'lib/mempool'
 import { TxEntity } from 'orm'
 import { getRepository } from 'typeorm'
+import config from 'config'
 
-export async function getTx(txhash: string): Promise<(Transaction.LcdTransaction & { chainId: string }) | undefined> {
+type FcdTx =
+  | (Transaction.LcdTransaction & { chainId: string })
+  | {
+      height: ''
+      txhash: string
+      tx: Transaction.LcdTx
+      timestamp: string // First seen at
+      chainId: string
+    }
+
+export async function getTx(txhash: string): Promise<FcdTx | undefined> {
+  const mempoolItem = Mempool.getTransactionByHash(txhash)
+
+  if (mempoolItem) {
+    return {
+      height: '',
+      txhash: mempoolItem.txhash,
+      tx: mempoolItem.lcdTx,
+      timestamp: new Date(mempoolItem.firstSeenAt).toISOString(),
+      chainId: config.CHAIN_ID
+    }
+  }
+
   const qb = getRepository(TxEntity)
     .createQueryBuilder()
     .where('hash=lower(:txhash) OR hash=upper(:txhash) ', { txhash })
