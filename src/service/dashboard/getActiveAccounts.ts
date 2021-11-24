ab85f4fefd6ce09f18377000626e33729171020e
Paul Kim <paul@terra.money>
2021-11-24 15:30:57 +0900

    feat(dashboard): use pre-indexed number for active accounts build: version 1.0.8

--- src/service/dashboard/getActiveAccounts.ts
+++ src/service/dashboard/getActiveAccounts.ts
@@ -1,33 +1,15 @@
-import { getConnection } from 'typeorm'
 import { DashboardEntity } from 'orm'
-import memoizeCache from 'lib/memoizeCache'
 import { getDashboardHistory } from './dashboardHistory'
-
-async function getTotalActiveAccountUncached(): Promise<number> {
-  const rawQuery = `SELECT COUNT(*) AS total_active_account FROM (SELECT DISTINCT account FROM account_tx) AS t`
-  const result = await getConnection().query(rawQuery)
-
-  return result.length ? Number(result[0].total_active_account) : 0
-}
-
-// TODO: Need a way to invalidate cache after 00:00
-const getTotalActiveAccount = memoizeCache(getTotalActiveAccountUncached, {
-  promise: true,
-  maxAge: 60 * 60 * 1000, // 1 hour cache
-  preFetch: 0.75 // fetch again after 45 mins
-})
 
 export default async function getActiveAccounts(): Promise<AccountStatReturn> {
   const dashboardHistory = await getDashboardHistory()
-  const total = await getTotalActiveAccount()
-
   const periodicActive = dashboardHistory.map((item: DashboardEntity) => ({
     datetime: item.timestamp.getTime(),
     value: item.activeAccount
   }))
 
   return {
-    total,
+    total: dashboardHistory.length ? dashboardHistory[dashboardHistory.length - 1].totalAccount : 0,
     periodic: periodicActive
   }
 }
